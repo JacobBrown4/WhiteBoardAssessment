ebf0e40f77c23e21891e9d5c8f22a50612afbace
JacobBrown4 <68622976+JacobBrown4@users.noreply.github.com>
2020-07-31 16:28:38 -0400

    initial commit

---
+++ PlaceForMethods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _99_WhiteBoardAssessment
+{
+    class PlaceForMethods
+    {
+        //Build my methods for part 2 & 3
+        public void RandomMethod(string name) {
+            Console.WriteLine("Does something " + name);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _99_WhiteBoardAssessment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           
+                PlaceForMethods placeForMethods = new PlaceForMethods();
+                placeForMethods.RandomMethod("Jacob");
+            Console.ReadLine();
+        }
+    }
+}
